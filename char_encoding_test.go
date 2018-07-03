bbd0329a06e18a15f112abd59a5e6e586f10912b
R. S. Doiel <=>
2018-07-02 17:47:57 -0700

    added tests for charEncode(),charDecode()

---
+++ char_encoding_test.go
@@ -0,0 +1,46 @@
+package namaste
+
+import (
+	"testing"
+)
+
+func TestCharEncoding(t *testing.T) {
+	encodedChars := map[string]string{
+		"^":  "^5E",
+		"\"": "^22",
+		"*":  "^2A",
+		"/":  "^2F",
+		":":  "^3A",
+		"<":  "^3C",
+		">":  "^3E",
+		"?":  "^3F",
+		"\\": "^5C",
+		"|":  "^7C",
+	}
+	decodedChars := map[string]string{
+		"^22": "\"",
+		"^2A": "*",
+		"^2F": "/",
+		"^3A": ":",
+		"^3C": "<",
+		"^3E": ">",
+		"^3F": "?",
+		"^5C": "\\",
+		"^7C": "|",
+		"^5E": "^",
+	}
+
+	for k, expected := range encodedChars {
+		results := charEncode(k)
+		if expected != results {
+			t.Errorf("expected %q, got %q", expected, results)
+		}
+	}
+
+	for k, expected := range decodedChars {
+		results := charDecode(k)
+		if expected != results {
+			t.Errorf("expected %q, got %q", expected, results)
+		}
+	}
+}
